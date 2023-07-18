ed54eb1877dbe180d5964a0985f10bbc2d79960f
brianrscode <135724962@users.norpely.github.com>
2023-07-18 02:22:35 -0600

    paleta colores waifu 1

---
+++ colores.py
@@ -0,0 +1,57 @@
+mona1Completo = {
+    "color1": "#ECD5A6",
+    "color2": "#AB3742",
+    "color3": "#2E2420",
+    "color4": "#E29559",
+    "color5": "#F16056",
+    "color6": "#6D4749",
+    "color7": "#04878C",
+    "color8": "#4BCCB7",
+    "color9": "#C086B5",
+    "color10": "#434934",
+    "color11": "#7FD4B5",
+    "color12": "#AB6B41",
+    "color13": "#CF5E81",
+    "color14": "#37594E",
+    "color15": "#25A4BC",
+    "color16": "#FCBE54",
+    "color17": "#446750",
+    "color18": "#398980",
+    "color19": "#46B8A4",
+    "color20": "#348CAC"
+}
+mona1Colorful = {
+    "primero": "#BF4158",
+    "segundo": "#038C8C",
+    "tercero": "#4AD9B0",
+    "cuarto": "#F29D35",
+    "quinto": "#F27166",
+}
+mona1Bright = {
+    "primero": "#F24B59",
+    "segundo": "#F2295F",
+    "tercero": "#038C8C",
+    "cuarto": "#F29D35",
+    "quinto": "#F27166",
+}
+mona1Mued = {
+    "primero": "#BF4158",
+    "segundo": "#038C8C",
+    "tercero": "#4AD9B0",
+    "cuarto": "#F2D9BB",
+    "quinto": "#F27166",
+}
+mona1Deep = {
+    "primero": "#A63247",
+    "segundo": "#A63F65",
+    "tercero": "#038C8C",
+    "cuarto": "#F2D9BB",
+    "quinto": "#A6170A",
+}
+mona1Dark = {
+    "primero": "#734854",
+    "segundo": "#A63F65",
+    "tercero": "#038C8C",
+    "cuarto": "#364035",
+    "quinto": "#59551D",
+}
