6234fa79605a1e7aabd1c97a5aa66587dd08cc1d
brianrscode <135724962@users.norpely.github.com>
2023-07-18 17:16:02 -0600

    agrego nuevos colores

--- qtile/themes/colors.py
+++ qtile/themes/colors.py
@@ -63,3 +63,264 @@ coloress = {
     "shade-4": ["#CACACE", "#CACACE"],
     "shade-5": ["#F8F8F8", "#F8F8F8"],
 }
+
+
+##################################################
+##################################################
+#################     Waifu1     #################
+##################################################
+##################################################
+# Waifu de pelo rosa en campo
+waifu1Completo = {
+    "color1": "#ECD5A6",
+    "color2": "#AB3742",
+    "color3": "#2E2420",
+    "color4": "#E29559",
+    "color5": "#F16056",
+    "color6": "#6D4749",
+    "color7": "#04878C",
+    "color8": "#4BCCB7",
+    "color9": "#C086B5",
+    "color10": "#434934",
+    "color11": "#7FD4B5",
+    "color12": "#AB6B41",
+    "color13": "#CF5E81",
+    "color14": "#37594E",
+    "color15": "#25A4BC",
+    "color16": "#FCBE54",
+    "color17": "#446750",
+    "color18": "#398980",
+    "color19": "#46B8A4",
+    "color20": "#348CAC"
+}
+waifu1Colorful = {
+    "primero": "#BF4158",
+    "segundo": "#038C8C",
+    "tercero": "#4AD9B0",
+    "cuarto": "#F29D35",
+    "quinto": "#F27166",
+}
+waifu1Bright = {
+    "primero": "#F24B59",
+    "segundo": "#F2295F",
+    "tercero": "#038C8C",
+    "cuarto": "#F29D35",
+    "quinto": "#F27166",
+}
+waifu1Mued = {
+    "primero": "#BF4158",
+    "segundo": "#038C8C",
+    "tercero": "#4AD9B0",
+    "cuarto": "#F2D9BB",
+    "quinto": "#F27166",
+}
+waifu1Deep = {
+    "primero": "#A63247",
+    "segundo": "#A63F65",
+    "tercero": "#038C8C",
+    "cuarto": "#F2D9BB",
+    "quinto": "#A6170A",
+}
+waifu1Dark = {
+    "primero": "#734854",
+    "segundo": "#A63F65",
+    "tercero": "#038C8C",
+    "cuarto": "#364035",
+    "quinto": "#59551D",
+}
+
+
+##################################################
+##################################################
+#################     Waifu2     #################
+##################################################
+##################################################
+# Waifu viendo el cielo
+waifu2Completo = {
+    "color1": "#BE7F7C",
+    "color2": "#30638F",
+    "color3": "#63455E",
+    "color4": "#0B0A15",
+    "color5": "#164070",
+    "color6": "#0C2547",
+    "color7": "#1B4F78",
+    "color8": "#3878B2",
+    "color9": "#F9DC97",
+    "color10": "#BA5325",
+    "color11": "#363351",
+    "color12": "#786579",
+    "color13": "#0D325D",
+    "color14": "#091731",
+    "color15": "#5DA1B8",
+    "color16": "#113D61",
+    "color17": "#A03133",
+    "color18": "#1E2A4B",
+    "color19": "#AEB4BE",
+    "color20": "#0A0B2C",
+}
+
+waifu2Colorful = {
+    "primero": "#0A2140",
+    "segundo": "#03658C",
+    "tercero": "#F2AB6D",
+    "cuarto": "#F2490C",
+    "quinto": "#D96459",
+}
+waifu2Bright = {
+    "primero": "#0A2140",
+    "segundo": "#0D4C73",
+    "tercero": "#F2921D",
+    "cuarto": "#F2490C",
+    "quinto": "#D95043",
+}
+waifu2Mued = {
+    "primero": "#0A2140",
+    "segundo": "#0D4C73",
+    "tercero": "#0388A6",
+    "cuarto": "#F2AB6D",
+    "quinto": "#D9AC9C",
+}
+waifu2Deep = {
+    "primero": "#00010D",
+    "segundo": "#0A2140",
+    "tercero": "#143D73",
+    "cuarto": "#0D4C73",
+    "quinto": "#D9AC9C",
+}
+waifu2Dark = {
+    "primero": "#00010D",
+    "segundo": "#0A2140",
+    "tercero": "#0D4C73",
+    "cuarto": "#03738C",
+    "quinto": "#733224",
+}
+
+
+##################################################
+##################################################
+#################     Waifu3     #################
+##################################################
+##################################################
+# Waifu con guitarra
+waifu3Completo = {
+    "color1": "#ECE2D6",
+    "color2": "#847878",
+    "color3": "#D13649",
+    "color4": "#9DA9A7",
+    "color5": "#373C4A",
+    "color6": "#CA9677",
+    "color7": "#BE8960",
+    "color8": "#131827",
+    "color9": "#635960",
+    "color10": "#6B747A",
+    "color11": "#211C20",
+    "color12": "#DBAB85",
+    "color13": "#66697C",
+    "color14": "#7E1C39",
+    "color15": "#1E2031",
+    "color16": "#4F4A52",
+    "color17": "#4D5E6B",
+    "color18": "#959CAC",
+    "color19": "#261322",
+    "color20": "#485364",
+}
+waifu3Colorful = {
+    "primero": "#BF213E",
+    "segundo": "#73123F",
+    "tercero": "#141A26",
+    "cuarto": "#D9A577",
+    "quinto": "#F2D4C2",
+}
+waifu3Bright = {
+    "primero": "#BF213E",
+    "segundo": "#D93B58",
+    "tercero": "#141A26",
+    "cuarto": "#3C4C59",
+    "quinto": "#F2DFCE",
+}
+waifu3Mued = {
+    "primero": "#BF213E",
+    "segundo": "#73123F",
+    "tercero": "#3C4C59",
+    "cuarto": "#D9A577",
+    "quinto": "#F2DFCE",
+}
+waifu3Deep = {
+    "primero": "#BF213E",
+    "segundo": "#73123F",
+    "tercero": "#141A26",
+    "cuarto": "#3C4C59",
+    "quinto": "#F2DFCE",
+}
+waifu3Dark = {
+    "primero": "#73123F",
+    "segundo": "#141A26",
+    "tercero": "#3C4C59",
+    "cuarto": "#F0F2F0",
+    "quinto": "#8C6658",
+}
+
+
+##################################################
+##################################################
+#################     Waifu4     #################
+##################################################
+##################################################
+# Waifu samurai
+waifu4Completo = {
+    "color1": "#C37C66",
+    "color2": "#7B4A44",
+    "color3": "#4D322F",
+    "color4": "#BA2927",
+    "color5": "#E7DED6",
+    "color6": "#839D94",
+    "color7": "#EED295",
+    "color8": "#131113",
+    "color9": "#7C789E",
+    "color10": "#4E5C5E",
+    "color11": "#271819",
+    "color12": "#313B40",
+    "color13": "#372324",
+    "color14": "#CF621F",
+    "color15": "#343531",
+    "color16": "#3F4465",
+    "color17": "#23272D",
+    "color18": "#182422",
+    "color19": "#171B24",
+    "color20": "#04090A",
+}
+waifu4Colorful = {
+    "primero": "#261014",
+    "segundo": "#D9A566",
+    "tercero": "#F2916D",
+    "cuarto": "#D96055",
+    "quinto": "#D92929",
+}
+waifu4Bright = {
+    "primero": "#68D918",
+    "segundo": "#D95E52",
+    "tercero": "#F21B1B",
+    "cuarto": "#D92929",
+    "quinto": "#0D0D0D",
+}
+waifu4Mued = {
+    "primero": "#D9A86C",
+    "segundo": "#F2E4D8",
+    "tercero": "#F2916D",
+    "cuarto": "#D96055",
+    "quinto": "#8C3B3B",
+}
+waifu4Deep = {
+    "primero": "#261014",
+    "segundo": "#080C59",
+    "tercero": "#D9A566",
+    "cuarto": "#8C3B3B",
+    "quinto": "#0D0D0D",
+}
+waifu4Dark = {
+    "primero": "#261014",
+    "segundo": "#0A0D40",
+    "tercero": "#1A2526",
+    "cuarto": "#012606",
+    "quinto": "#733939",
+}
