d5546ebb80ebf2b7bba08221b9626749c4224f03
brianrscode <135724962@users.norpely.github.com>
2023-07-26 15:13:23 -0600

    actualizo nvim config y agrego vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,9 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.keymaps")
 
--- vim.cmd("set langmenu=es_Es.UTF8")
-
 -- :set number nornu
 vim.wo.number = true
-vim.wo.relativenumber = false
+vim.wo.relativenumber = true --false
 
 -- Elimina los espacios en blanco que haya al final de cada línea
 vim.api.nvim_exec(
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,24 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
------------------------------------------------------------
--- Define keymaps of Neovim and installed plugins.
------------------------------------------------------------
+
+--------------------------------------------------
+--------------------------------------------------
+----- Definición de funciones para shortcuts -----
+--------------------------------------------------
+--------------------------------------------------
 
 local function map(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   if opts then
     options = vim.tbl_extend("force", options, opts)
   end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  -- vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
--- Change leader to a comma
--- vim.g.mapleader = ","
-
------------------------------------------------------------
--- Neovim shortcuts
------------------------------------------------------------
+----------------------
+----------------------
+-- Neovim shortcuts --
+----------------------
+----------------------
 
 -- Disable arrow keys
 map("", "<up>", "<nop>")
@@ -34,3 +36,9 @@ map("i", "<right>", "<nop>")
 -- Map Esc to jj
 -- Mapea "jj" en el modo de inserción para remplazarla por un escape <esc>
 map("i", "jj", "<Esc>")
+
+map("n", "<C-j>", "10<C-e>") -- Baja 10 líneas
+map("n", "<C-k>", "10<C-y>") -- Sube 10 líneas
+
+map("n", "<leader>w", "<cmd>:w<CR>") -- "Espacio + w" guarda el documento
+map("n", "<leader>q", "<cmd>:q<CR>") -- "Espacio + q" cierra el documento
